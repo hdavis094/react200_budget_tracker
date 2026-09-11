ba1b19b1c16bca82fedccb684c1a926c933ecb68
hdavis094 <hdavis094@gmail.com>
2026-09-10 17:27:00 -0700

    add delete button for income

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,21 +1,37 @@
 export const initialState = {
-    incomes: [],
-    expenses:[]
+  incomes: [],
+  expenses: [],
 };
 
 export function appReducer(state, action) {
-    switch (action.type) {
-        case 'ADD_INCOME':
-            return {
-                ...state,
-                incomes: [...state.incomes, action.payload]
-            };
-        case 'ADD_EXPENSE':
-            return {
-                ...state,
-                expenses: [...state.expenses,action.payload]
-            };
-            default:
-                return state;
-        }
-    }
+  switch (action.type) {
+    case "ADD_INCOME":
+      return {
+        ...state,
+        incomes: [...state.incomes, { ...action.payload, id: Date.now() }],
+      };
+    case "ADD_EXPENSE":
+      return {
+        ...state,
+        expenses: [...state.expenses, { ...action.payload, id: Date.now() }],
+      };
+    case "DELETE_INCOME":
+      return {
+        ...state,
+        incomes: state.incomes.filter((i) => i.id !== action.payload.id),
+      };
+    case "DELETE_EXPENSE":
+      return {
+        ...state,
+        expenses: state.expenses.filter((i) => i.id !== action.payload.id),
+      };
+    // case 'EDIT_INCOME':
+    //     return { ...state, incomes: state.incomes.map(i => i.id === action.payload.id ? { ...i, ...action.payload } : i)
+    // };
+    // case 'EDIT_EXPENSE':
+    //     return { ...state, expenses: state.expenses.map(i => i.id === action.payload.id ? { ...i, ...action.payload } : i )
+    // };
+    default:
+      return state;
+  }
+}
